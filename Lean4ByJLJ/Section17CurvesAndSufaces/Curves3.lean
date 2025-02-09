39f56d1349837a29c8bc0737a941e8bac23bc2e8
longjian <longjianj55@gmail.com>
2025-02-09 15:42:13 +0100

    an error pending to correct.

--- Lean4ByJLJ/Section17CurvesAndSufaces/Curves3.lean
+++ Lean4ByJLJ/Section17CurvesAndSufaces/Curves3.lean
@@ -4,6 +4,8 @@ import Mathlib.Analysis.Calculus.ContDiff.Defs
 import Mathlib.Analysis.SpecialFunctions.Trigonometric.Deriv
 import Mathlib.Analysis.Calculus.Deriv.Abs
 import Mathlib.Analysis.Calculus.InverseFunctionTheorem.ContDiff
+import Mathlib.Topology.Basic
+import Mathlib.Topology.Homeomorph
 
 /-
 
@@ -24,6 +26,7 @@ example : ContDiffOn ℝ ⊤ φ₁ (Set.Icc 0 1) := by
 
 
 open Real
+open Set
 
 noncomputable def φ₂ : ℝ → ℝ × ℝ × ℝ := fun x => (Real.sin x, x ^ 4 + 37 * x ^ 2 + 1, abs x)
 
@@ -66,7 +69,94 @@ example (φ : ℝ → ℝ) (ψ : ℝ → ℝ) (a b c d : ℝ) (hab : a ≤ b) (h
     (hφregular : ∀ x, x ∈ Set.Icc a b → fderiv ℝ φ x ≠ 0) :
     ContDiffOn ℝ ⊤ ψ (Set.Icc c d) ∧
       ∀ y, y ∈ Set.Icc c d → ∀ z, fderiv ℝ ψ y (fderiv ℝ φ (ψ y) z) = z := by
+
+      have hψ_hom : ∀ y ∈ Set.Icc c d, ∃ z ∈ Set.Icc a b, φ z = y := by
+        intro y hy
+        use ψ y
+        constructor
+        apply hψ at hy
+        exact hy
+        apply right_inv at hy
+        exact hy
+
+      have φ_injective_on_Icc : Set.InjOn φ (Set.Icc a b) := by
+        intro x hx y hy hxy
+        apply left_inv at hx
+        rw [hxy] at hx
+        apply left_inv at hy
+        rw [← hx]
+        exact hy
+      have φ_inv_of_ψ : Set.InvOn φ ψ (Set.Icc c d) (Set.Icc a b) := by
+        constructor
+        exact right_inv
+        exact left_inv
+
+      have RR_hom : ℝ ≃ₜ ℝ := by
+        exact Homeomorph.refl ℝ
+
+      have φ'regular : ∀ x ∈ Set.Icc a b, φ' x ≠ 0 := by
+        intro x hx
+        apply hφregular
+        exact hx
+
+
+      have hψdiff : ContDiffOn ℝ ⊤ ψ (Icc c d) := by
+        apply Homeomorph.contDiff_symm_deriv RR_hom φ'regular hψ_hom φ_injective_on_Icc φ_inv_of_ψ
+
       constructor
+      exact hψdiff
+
+      intro y hy z
+      apply hψ_hom at hy
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/-
+      have hφ_local_inv : ∀ x ∈ Set.Icc a b, HasFDerivAt φ (fderiv ℝ φ x) x := by
+        intro x hx
+        apply DifferentiableOn.hasFDerivAt
+        apply ContDiffOn.differentiableOn hφdiff
+        simp
+        rw [mem_nhds_iff]
+        cases' hx with hax hbx
+
+      constructor
+      apply ContDiffOn.of_local_inverse
+
+      intro y hy z
+      have hφψ : φ (ψ y) = y := right_inv y hy
+      have hψφ : ψ (φ (ψ y)) = ψ y := congr_arg ψ hφψ
+      rw [left_inv (ψ y) (hψ y hy)] at hψφ
+
+      have hψ_deriv : HasFDerivAt ψ (fderiv ℝ ψ y) y := by
+        apply DifferentiableOn.hasFDerivAt
+        apply ContDiffOn.differentiableOn hψdiff
+        simp
+        rw [mem_nhds_iff]
+        cases' hy with hcy hdy
+
+      have hφ_deriv : HasFDerivAt φ (fderiv ℝ φ (ψ y)) (ψ y) := by
+        apply hφ_local_inv
+        apply hψ
+        exact hy
+-\
 
 
 /-
